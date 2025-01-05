7e63b99dc339e451cc38155c8ffa57d6470087f3
dgohks <derekgohderek62@gmail.com>
2025-01-05 23:10:26 +0800

    Testing

--- utils/solana.ts
+++
@@ -1,6 +0,0 @@
-import { Connection, clusterApiUrl } from '@solana/web3.js';
-
-export const getConnection = () => new Connection(
-  process.env.NEXT_PUBLIC_SOLANA_RPC_ENDPOINT || clusterApiUrl('devnet')
-);
-
